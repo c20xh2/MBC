21c98a432ded55872b86cf80fffd5df62ee2f5d9
c20xh2 <c20xh2@gmail.com>
2019-06-14 14:30:26 -0400

    caca

--- find_text.py
+++ find_text.py
@@ -1,1 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
+
+PAGE_COUNT = 1
+PAGE_MAX = 113
+
+
+
+def trouve_des_links_de_vidange(PAGE_COUNT):
+	url = 'https://www.journaldemontreal.com/auteur/mathieu-bock-cote/page/{}'.format(PAGE_COUNT)
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text,'html')
+	print(soup.text)
+
+
+
+
+while PAGE_COUNT <= PAGE_MAX:
+	trouve_des_links_de_vidange(PAGE_COUNT)
+	PAGE_COUNT += 1
